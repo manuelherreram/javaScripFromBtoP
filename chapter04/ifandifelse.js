4b1fe5074a08f9cc054b6274b3349ab48b9fad8a
Manuel Herrera <manuel.herrera.m@gmail.com>
2023-04-23 23:15:02 -0400

    If conditional

---
+++ chapter04/ifandifelse.js
@@ -0,0 +1,37 @@
+let rain = true;
+if (rain) {
+  console.log("** Take my umbrella when I need to go outside **");
+} else {
+  console.log("** I can leave my umbrella at home **");
+}
+
+let age = 17;
+if (age < 18) {
+  console.log("We are sorry, but you can't get in under 18");
+} else {
+  console.log("Welcome!");
+}
+
+let hobby = "dancing";
+if (hobby === "coding") {
+  console.log("I love coding too!");
+} else {
+  console.log("Can you teach me that?");
+}
+
+// Practice
+// Create a variable with a Boolean value.
+// Output the value of the variable to the console.
+// Check whether the variable is true and if so, output a message to the console, using the following syntax:
+// if(myVariable){
+// //action
+// }
+// Add another if statement with an ! in front of the variable to check whether the condition is not true, and create a message that will be printed to the console in that instance. You should have two if statements, one with an ! and the other without. You could also use an if and an else statement instead—experiment!
+// Change the variable to the opposite to see how the result changes.
+let sunny = false;
+if (sunny) {
+  console.log("Don't forget to use sun protector");
+} // if (!sunny) - another option
+else {
+  console.log("It's not necessary to use sun protector");
+}
