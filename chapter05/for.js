aee998069993584680f430a341eba9531ab69071
Manuel Herrera <manuel.herrera.m@gmail.com>
2023-05-03 14:33:37 -0400

    for loops

--- chapter05/for.js
+++ chapter05/for.js
@@ -32,20 +32,20 @@ console.table(arrayOfArrays);
 
 //Practice 5.4
 
-const myTable = [];
-const row = 4;
-const column = 7;
-let counter = 0;
-for (let i = 0; i < row; i++) {
-  let tempTable = [];
+// const myTable = [];
+// const row = 4;
+// const column = 7;
+// let counter = 0;
+// for (let i = 0; i < row; i++) {
+//   let tempTable = [];
 
-  for (let j = 0; j < column; j++) {
-    counter++;
-    tempTable.push(counter);
-  }
-  myTable.push(tempTable);
-}
-console.table(myTable);
+//   for (let j = 0; j < column; j++) {
+//     counter++;
+//     tempTable.push(counter);
+//   }
+//   myTable.push(tempTable);
+// }
+// console.table(myTable);
 
 // for arrays
 let names = ["Manuel", "Daniela", "Olivia", "Rayen", "Emilia"];
@@ -58,3 +58,54 @@ for (let i = 0; i < names.length; i++) {
   names[i] = "Hello " + names[i];
 }
 console.log(names);
+
+// Practice 5.5
+const grid = [];
+const cells = 64;
+let counter = 0;
+let row;
+for (let x = 0; x < cells + 1; x++) {
+  if (counter % 8 == 0) {
+    if (row != undefined) {
+      grid.push(row);
+    }
+    row = [];
+  }
+  counter++;
+  let temp = counter;
+  row.push(temp);
+}
+console.table(grid);
+
+// for of
+let names2 = ["Manuel", "Daniela", "Olivia", "Rayen", "Emilia"];
+for (let name of names2) {
+  console.log(name);
+}
+
+let arr = [];
+for (let i = 0; i < 10; i++) {
+  arr.push(i + 1);
+}
+console.log(arr);
+
+for (let j = 0; j < arr.length; j++) {
+  console.log(arr[j]);
+}
+for (let val of arr) {
+  console.log(val);
+}
+
+// for in
+
+let car = {
+  model: "Golf",
+  make: "VW",
+  year: 1999,
+  color: "black",
+};
+
+for (let prop in car) {
+  //console.log(car[prop]);
+  console.log(prop);
+}
